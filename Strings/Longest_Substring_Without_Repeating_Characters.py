497b81556b82373debddb16f1fdd904f775a8827
Michael <michael.patel94@gmail.com>
2022-02-16 20:09:43 -0500

    Adding longest substring problem

---
+++ Strings/Longest_Substring_Without_Repeating_Characters.py
@@ -0,0 +1,21 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        # initialize
+        max_length = 0
+
+        for i in range(len(s)):  # first pointer
+            substring = s[i]  # reset
+            characters = {}  # reset
+            characters[s[i]] = True
+            j = i + 1  # second pointer
+
+            while (j < len(s)) and (s[j] not in characters):
+                characters[s[j]] = True
+                substring += s[j]
+                j += 1
+
+            # found a repeat character
+            if len(substring) > max_length:
+                max_length = len(substring)
+
+        return max_length
