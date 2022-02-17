17f053fde7e5f02bd0fcc3acbc5671c00cdfe294
Michael <michael.patel94@gmail.com>
2022-02-16 20:29:23 -0500

    Formatting for clarity

--- Arrays/Median_of_Two_Sorted_Arrays.py
+++ Arrays/Median_of_Two_Sorted_Arrays.py
@@ -12,13 +12,16 @@ class Solution:
 
         if arr_length == 0:  # no elements
             return 0
+
         elif arr_length == 1:  # 1 element
             return arr[0]
+
         else:  # at least 2 elements
             if arr_length % 2 == 0:  # even
                 median_index = int(arr_length / 2)
                 total = arr[median_index - 1] + arr[median_index]
                 return float(total / 2)
+            
             else:  # odd
                 median_index = int(arr_length / 2)
                 return arr[median_index]
