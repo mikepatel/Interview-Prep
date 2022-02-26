175592ee4b162e258af2fdfc9d49c33b64007ad6
Michael <michael.patel94@gmail.com>
2022-02-26 18:19:09 -0500

    Making 'candidate' type string

--- Backtracking/Generate Parentheses.py
+++ Backtracking/Generate Parentheses.py
@@ -16,8 +16,7 @@ class Solution:
 
             # found a solution, add to 'combos'
             if len(candidate) == 2 * n:
-                solution = "".join(candidate)
-                combos.append(solution)
+                combos.append(candidate)
                 return
 
             else:  # continue searching
@@ -25,32 +24,32 @@ class Solution:
                 if num_left_parentheses < n:
                     # apply value
                     # add a '(' --> add a new pair
-                    candidate.append("(")
+                    candidate = candidate + "("
                     num_left_parentheses += 1
 
                     # search for solution
                     backtrack(candidate, num_left_parentheses, num_right_parentheses)
 
                     # remove value
-                    candidate.pop()
+                    candidate = candidate[:-1]
                     num_left_parentheses = num_left_parentheses - 1
 
                 if num_right_parentheses < num_left_parentheses:
                     # apply value
                     # add a ')' for balance
-                    candidate.append(")")
+                    candidate = candidate + ")"
                     num_right_parentheses += 1
 
                     # search for solution
                     backtrack(candidate, num_left_parentheses, num_right_parentheses)
 
                     # remove value
-                    candidate.pop()
+                    candidate = candidate[:-1]
                     num_right_parentheses = num_right_parentheses - 1
 
         #
         # driver code
         combos = []
-        backtrack(candidate=[], num_left_parentheses=0, num_right_parentheses=0)
+        backtrack(candidate="", num_left_parentheses=0, num_right_parentheses=0)
 
         return combos
