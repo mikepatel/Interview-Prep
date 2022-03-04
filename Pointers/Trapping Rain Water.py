e3c253fc04c5377612bcb506d734d4273e490022
Michael <michael.patel94@gmail.com>
2022-03-03 19:24:17 -0500

    Adding a two pointers problem

---
+++ Pointers/Trapping Rain Water.py
@@ -0,0 +1,43 @@
+"""
+https://leetcode.com/problems/trapping-rain-water/
+"""
+
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+
+        # TWO POINTERS --> while-loop
+
+        # initialize
+        total = 0
+        left = 0  # left pointer
+        right = len(height) - 1  # right pointer
+
+        max_height_left = 0
+        max_height_right = 0
+
+        while left < right:
+            current_height_left = height[left]
+            current_height_right = height[right]
+
+            if current_height_left < current_height_right:
+                if current_height_left > max_height_left:
+                    max_height_left = current_height_left
+
+                else:  # current_height_left <= max_height_left
+                    # add area to total
+                    total += max_height_left - current_height_left
+
+                left = left + 1  # move left pointer
+
+            else:  # current_height_left <= current_height_right
+                if current_height_right > max_height_right:
+                    max_height_right = current_height_right
+
+                else:  # current_height_right <= max_height_right
+                    # add area to total
+                    total += max_height_right - current_height_right
+
+                right = right - 1  # move right pointer
+
+        return total
