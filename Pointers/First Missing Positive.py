181dd5b7ac49a96c4363c0145a9d65855b1aed03
Michael <michael.patel94@gmail.com>
2022-03-03 19:00:13 -0500

    Adding a hashmap problem

---
+++ Pointers/First Missing Positive.py
@@ -0,0 +1,23 @@
+"""
+https://leetcode.com/problems/first-missing-positive/
+"""
+
+
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        # Approach 1
+        # build map
+        mapper = {}
+        for n in nums:
+            if n > 0:
+                if n not in mapper:
+                    mapper[n] = True
+
+        # check if mapper is empty
+        if not mapper:
+            return 1
+
+        # find missing
+        for i in range(1, 2 ** 31):
+            if i not in mapper:
+                return i
