d58c3fd7219d84408796d24d07ab71c0bd94174e
Michael <michael.patel94@gmail.com>
2022-03-15 19:54:21 -0400

    Adding notes

--- Pointers/Jump Game.py
+++ Pointers/Jump Game.py
@@ -6,15 +6,15 @@ https://leetcode.com/problems/jump-game/
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         # POINTER
-        # MOVE BACKWARDS: target --> 0 instead of 0 --> target
+        # MOVE BACKWARDS: target --> 0, instead of 0 --> target
         target_index = len(nums) - 1
 
         # move backwards
         for i in range(len(nums) - 1, -1, -1):
-            if i + nums[i] >= target_index:
-                target_index = i  # reset
+            if i + nums[i] >= target_index:  # can I make the jump from here?
+                target_index = i  # reset --> move further back
 
-        if target_index == 0:
+        if target_index == 0:  # reached the beginning
             return True
 
         else:
