4a349f3c4eec4aa4dead0b9d9d67b5d91311d40d
Michael <michael.patel94@gmail.com>
2022-03-15 19:44:02 -0400

    Adding notes

--- Pointers/First Missing Positive.py
+++ Pointers/First Missing Positive.py
@@ -7,17 +7,32 @@ class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         # Approach 1
         # build map
+        """
         mapper = {}
         for n in nums:
-            if n > 0:
+            if n>0:
                 if n not in mapper:
                     mapper[n] = True
 
-        # EDGE CASE: check if mapper is empty
+        # check if mapper is empty
         if not mapper:
             return 1
 
         # find missing
-        for i in range(1, 2 ** 31):
+        for i in range(1, 2**31):
             if i not in mapper:
                 return i
+        """
+
+        # Approach 2
+        #
+        # eliminate all values less than 1
+        nums = set(nums)  # eliminate duplicates
+        smallest = 1
+        for n in nums:
+            if n > 0:
+                if n == smallest:
+                    smallest = smallest + 1
+
+        return smallest
+
