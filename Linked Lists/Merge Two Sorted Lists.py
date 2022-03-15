9e266f8ce59c32cb2ad00d6e78f6f8e8acddcf73
Michael <michael.patel94@gmail.com>
2022-03-15 18:34:25 -0400

    Updating 'output' to 'current'

--- Linked Lists/Merge Two Sorted Lists.py
+++ Linked Lists/Merge Two Sorted Lists.py
@@ -12,26 +12,26 @@ class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
 
         dummy = ListNode()  # dummy head
-        output = dummy
+        current = dummy
 
         while list1 and list2:  # while both are non-empty
             if list1.val <= list2.val:
-                output.next = list1  # set next node
+                current.next = list1  # set next node
 
-                output = output.next  # advance
+                current = current.next  # advance
                 list1 = list1.next  # advance
 
             else:
-                output.next = list2  # set next node
+                current.next = list2  # set next node
 
-                output = output.next  # advance
+                current = current.next  # advance
                 list2 = list2.next  # advance
 
         # at least one is still non-empty
         if list1:
-            output.next = list1
+            current.next = list1
 
         else:
-            output.next = list2
+            current.next = list2
 
         return dummy.next
