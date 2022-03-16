6bf1af8ab3e57857698972451f9935c6967a750a
Michael <michael.patel94@gmail.com>
2022-03-15 20:06:26 -0400

    Adding notes

--- Pointers/Maximum Subarray.py
+++ Pointers/Maximum Subarray.py
@@ -5,41 +5,21 @@ https://leetcode.com/problems/maximum-subarray/
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-
-        # Approach 1: TWO POINTERS, dynamic programming
         """
-        maximum = -10**4  # from constraints
-
-        for i in range(len(nums)):
-            current = nums[i]
-
-            if current > maximum:
-                maximum = current
-
-            j = i+1
-            while j < len(nums):
-                current = current + nums[j]
-                j += 1
-
-                if current > maximum:
-                    maximum = current
-
-        return maximum
+        Find starting point from which to start summing
         """
-
-        # Approach 2:
-        total = nums[0]
-        maximum = nums[0]
+        #
+        total = nums[0]  # first value
+        maximum = nums[0]  # first value
 
         for i in range(1, len(nums)):
             current = nums[i]
 
-            # does adding the next number increase the total
-            if total + current > current:
-                total = total + current
+            if total <= 0:
+                total = current  # reset total
 
-            else:  # adding the next number decreases the total
-                total = current  # reset starting point
+            else:  # total > 0
+                total = total + current
 
             if total > maximum:
                 maximum = total
