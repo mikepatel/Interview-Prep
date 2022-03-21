22886f7944fbcda8f8d715569298a3262c499eb2
Michael <michael.patel94@gmail.com>
2022-03-21 11:18:39 -0400

    Fixing typo

--- Pointers/Remove Duplicates from Sorted Array.py
+++ Pointers/Remove Duplicates from Sorted Array.py
@@ -8,14 +8,16 @@ class Solution:
         # REQ: in-place modification
 
         # approach 1: python functions
-        nums[:] = sorted(list(set(nums)))
+        # nums[:] = sorted(list(set(nums)))
 
         # approach 2: pointer --> while loop
         i = 0
         while i < len(nums) - 1:
             if nums[i] == nums[i + 1]:
-                nums.pop(i)
+                nums.pop(i + 1)
 
-            i += 1
+            else:
+                i += 1
 
         return len(nums)
+
